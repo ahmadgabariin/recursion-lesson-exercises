09aa5c486ff187a2b59690f720b3687afd931b36
ahmadgabariin <ahmadhamze.kawasaki@gmail.com>
2022-05-25 18:56:45 +0300

    extension task added

--- src/play.js
+++ src/play.js
@@ -1,16 +1,3 @@
-const arr1 = [1, 2, 3,77,6]
-const arr2 = []
-
-const swap = function(arr1 ,arr2) {
-    if (arr1.length === 0) { 
-        return 
-    }
-    let savedCell = arr1[arr1.length - 1]
-    arr1.pop()
-    swap(arr1,arr2)
-    arr2.push(savedCell)
-}
-
-swap(arr1 , arr2)
-console.log(arr1) //[]
-console.log(arr2) //[1, 2, 3] 
+let arr1 =[1,2,3]
+console.log(arr1.splice(0))
+console.log(arr1)
